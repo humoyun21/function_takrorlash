88da30bc1a009ed624a238fe7b2663a3b122ea51
humoyun21 <husanovhumoyunbek20052109@gmail.com>
2024-01-19 01:44:09 +0500

    function

---
+++ main.js
@@ -0,0 +1,122 @@
+/*
+1-masala
+
+function numberDegree(n) {
+  console.log(n ** 3);
+}
+numberDegree(3);
+
+2-masala
+
+function numberDegree(a ,b) {
+  console.log(a + b)
+}
+numberDegree(3,6); 
+
+
+3-masala
+
+function numberDegree(a ,b) {
+  console.log(a -b)
+}
+numberDegree(3,3);
+
+4-masala
+
+function numberDegree(a) {
+  console.log(a ** 2);
+  console.log(a ** 3);
+  console.log(a ** 4);
+}
+numberDegree(3);
+
+5-maslaa
+
+
+function numberOdd(n) {
+    for(let i = 1; i <= n; i++){
+      if(i%2==1){
+        console.log(i)
+      }
+    }
+}
+numberOdd(20)
+
+6-masla
+
+function numberOdd(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 == 0) {
+      console.log(i);
+    }
+  }
+}
+numberOdd(20);
+
+7-masla
+let a = "";
+let b = "";
+function number(n) {
+  console.log(n - 1);
+  console.log(n + 1);
+}
+number(9);
+
+8-masla
+
+function boluvchi(n) {
+  for (let i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      console.log(i);
+    }
+  }
+}
+boluvchi(21);
+
+
+9-masala
+ 
+function calculate(num1, num2, operator) {
+  let result;
+  
+  if (operator === '+') {
+    result = num1 + num2;
+  } else if (operator === '-') {
+    result = num1 - num2;
+  } else if (operator === '/') {
+    result = num1 / num2;
+  } else if (operator === '*') {
+    result = num1 * num2;
+  } else {
+    console.log("Noto'g'ri operator!");
+    return;
+  }
+  
+  console.log('Natija:', result);
+}
+
+calculate(5, 3, '+');
+
+*/
+
+/*function yigindi(n) {
+  let yigindi = 0;
+  for (let i = 1; i <= n; i++) {
+    yigindi += 1 / i;
+  }
+  return yigindi;
+}
+console.log(yigindi(2));
+
+
+10-msala
+function printMultiplicationTable() {
+  for (let i = 1; i <= 9; i++) {
+    for (let j = 1; j <= 10; j++) {
+      console.log(i * j);
+    }
+  }
+}
+
+printMultiplicationTable();
+*/
